471ceb814a1839a9858bceb9702ac661e8f37e6d
SLDowney <sdowney@outlook.com>
2021-12-08 17:23:09 -0330

    website functional. Tweaking formatting on index.html and others.

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 import sqlite3
 from contextlib import closing
-from flask import Flask, render_template, redirect, request, abort
-import imghdr
+from flask import Flask, render_template, request, abort
 import os
 from werkzeug.utils import secure_filename
 
@@ -15,12 +14,19 @@ conn = sqlite3.connect("Collection.db", check_same_thread=False)
 @app.route("/")
 def index():
     headline = "This is a Test!"
+    with closing(conn.cursor()) as c:
+        query = '''Select * From Collection'''
+        c.execute(query)
+        results = c.fetchall()
+        images = []
+        for image in images:
+            images.append((image[0], image[1], image[2], image[3]))
     return render_template("index.html", headline=headline)
 
 
 @app.route("/display")
 def display():
-    headline = "This is the Display Page!"
+    headline = "View your Collection in all it's glory!!"
     with closing(conn.cursor()) as c:
         query = '''Select * From Collection'''
         c.execute(query)
@@ -31,14 +37,14 @@ def display():
     return render_template("display.html", headline=headline, images=images)
 
 
-@app.route("/additem")
-def additem():
+@app.route("/success")
+def success():
     headline = "Choose a file to upload to the collection:"
-    return render_template("additem.html", headline=headline)
+    return render_template("success.html", headline=headline)
 
 
 @app.route("/additem", methods=["GET", "POST"])
-def addItemToCollection():
+def additem():
     headline = "Choose a file to upload to the collection:"
     if request.method == 'POST':
         uploaded_file = request.files["item_picture"]
@@ -58,48 +64,48 @@ def addItemToCollection():
                             VALUES(?, ?, ?, ?)'''
                 c.execute(query, (filename, title, author, series))
                 conn.commit()
-                headline = "success!!"
+                tagline = "Successfully added Item to Collection!!"
+                return render_template("success.html", tagline=tagline)
         except sqlite3.OperationalError as e:
             print(e)
-            headline = "error in insert operation"
+            headline = "Error in insert operation. Please try again."
+    else:
         return render_template("additem.html", headline=headline)
 
 
-# @app.route("/deleteitem")
-# def deleteitem():
-#     headline = "File Successfully Deleted!"
-#     return render_template("deleteitem.html", headline=headline)
-#
-
 @app.route("/deleteitem", methods=["GET", "POST"])
 def deleteitem():
-    headline = "This is the Delete Item Page!"
+    counter = 0
+    headline = "Choose a file to delete from the collection:"
     with closing(conn.cursor()) as c:
-        query = '''SELECT item_title FROM Collection'''
+        query = '''SELECT * FROM Collection'''
         c.execute(query)
         items = c.fetchall()
         item_titles = []
         for item in items:
             item_titles.append(item)
     if request.method == 'POST':
-        itemToDelete = int(request.form['items'])
-        i = 0
-        while i < len(item_titles):
-            if itemToDelete == item[2:-3]:
+        itemToDelete = int(request.form['item'])
+        for item in item_titles:
+            if itemToDelete == item[4]:
                 try:
                     with closing(conn.cursor()) as c:
                         query = '''DELETE FROM Collection
-                                    WHERE item_title = (?)'''
+                                    WHERE item_index = ?'''
                         c.execute(query, (itemToDelete,))
                         conn.commit()
-                        headline = "success!!"
+                        item_titles.pop(counter)
+                        if os.path.exists("static\images\\" + item[0]):
+                            os.remove("static\images\\" + item[0])
+                            tagline = "Successfully Deleted Item from Collection!!"
+                        else:
+                            tagline = "Failed to delete image file"
+                        return render_template("success.html", tagline=tagline)
                 except sqlite3.OperationalError as e:
                     print(e)
-                    headline = "error in insert operation"
-            i = i + 1
-
-        # title = request.form['title']
-    return render_template("deleteitem.html", headline=headline, item_titles=item_titles)
+                    headline = "Error in delete operation. Please try again.", e
+            counter = counter + 1
+    return render_template("deleteitem.html", headline=headline, item_titles=item_titles, counter=counter)
 
 
 if __name__ == "__main__":
